9f34489d5336f653c6d7d9fae94e551849444bc6
BilkaDev <bilkadev@gmail.com>
2023-11-22 11:13:38 +0100

    feat: Added button

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react'
+import Button from "@/shared/components/uiElements/Button";
+
+interface PageProps {
+}
+
+const Page: FC<PageProps> = () => {
+    return (
+        <Button>Hello</Button>
+    )
+        ;
+}
+
+export default Page;
